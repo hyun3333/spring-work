5d3fa31dd5d04dabfdd43e8aafa85d05d23e430e
hyun <hyun231366@gmail.com>
2023-05-15 18:17:46 +0900

    MyWeb 초안

---
+++ MyWeb/src/main/java/com/spring/myweb/command/FreeBoardVO.java
@@ -0,0 +1,30 @@
+package com.spring.myweb.command;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/*
+ CREATE TABLE jdbc_board(
+	board_no INT PRIMARY KEY AUTO_INCREMENT,
+    writer VARCHAR(30) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(2000),
+    reg_date DATETIME DEFAULT CURRENT_TIMESTAMP
+); 
+ */
+@Getter 
+@Setter
+@ToString
+public class FreeBoardVO {
+
+	private int bno;
+	private String title;
+	private String writer;
+	private String content;
+	private LocalDateTime regDate;
+	private LocalDateTime updateDate;
+	
+}
