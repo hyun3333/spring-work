fbe98dc8738fee916b59cd8531c833deac320046
hyun <hyun231366@gmail.com>
2023-05-16 18:58:01 +0900

    에러 해결

--- MyWeb/src/main/java/com/spring/myweb/FreeBoardController.java
+++ MyWeb/src/main/java/com/spring/myweb/FreeBoardController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,8 +39,8 @@ public class FreeBoardController {
 	}
 	
 	//글 상세 보기 처리
-	@GetMapping("/content")
-	public String getConString(int bno, Model model) {
+	@GetMapping("/content/{bno}")
+	public String getConString(@PathVariable int bno, Model model) {
 		model.addAttribute("article", service.getContent(bno));
 		return "freeboard/freeDetail";
 	}
@@ -55,7 +56,7 @@ public class FreeBoardController {
 	@PostMapping("/update")
 	public String update(FreeBoardVO vo) {
 		service.update(vo);
-		return "redirect:/freeboard/content?bno=" + vo.getBno();
+		return "redirect:/freeboard/content/" + vo.getBno();
 	}
 	
 	//글 삭제 처리
