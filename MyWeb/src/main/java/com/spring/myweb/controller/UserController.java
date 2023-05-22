151bd46cb00d21f1d2e31b865325f110c54b3512
hyun <hyun231366@gmail.com>
2023-05-22 11:10:27 +0900

    login 로직 수정중

--- MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
+++ MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.spring.myweb.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.spring.myweb.command.UserVO;
 import com.spring.myweb.user.service.IUserService;
 import com.spring.myweb.util.MailSenderService;
+import com.spring.myweb.util.PageVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -72,7 +76,11 @@ public class UserController {
 	
 	// 마이페이지 이동 요청
 	@GetMapping("/userMypage")
-	public void userMypage() {
+	public void userMypage(HttpSession session, Model model, PageVO vo) {
+		
+		// 세션 데이터에서 id를 뽑아야 sql을 돌릴 수 있다.
+		String id = (String) session.getAttribute("login");
+		UserVO user = service.getInfo(id, vo);
 		
 	}
 
