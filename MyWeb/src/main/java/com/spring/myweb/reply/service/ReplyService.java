2ac9896eec319d92c8119094baab745dfafc18c4
hyun <hyun231366@gmail.com>
2023-05-23 15:20:41 +0900

    댓글 수정하기 눌림버튼 완성

--- MyWeb/src/main/java/com/spring/myweb/reply/service/ReplyService.java
+++ MyWeb/src/main/java/com/spring/myweb/reply/service/ReplyService.java
@@ -17,7 +17,7 @@ public class ReplyService implements IReplyService {
 
 	@Autowired
 	private IReplyMapper mapper;
-	
+	@Autowired
 	private BCryptPasswordEncoder encoder;
 	
 	@Override
@@ -30,20 +30,19 @@ public class ReplyService implements IReplyService {
 	public List<ReplyVO> getList(int bno, int pageNum) {
 		
 		PageVO vo = new PageVO();
-		vo.setPageNum(pageNum); // 화면에서 전달된 페이지 번호
-		vo.setCpp(5); // 댓글은 한 화면에 5개씩.
+		vo.setPageNum(pageNum); //화면에서 전달된 페이지 번호
+		vo.setCpp(5); //댓글은 한 화면에 5개씩.
 		
 		Map<String, Object> data = new HashMap<>();
-		data.put("paging", vo); // 페이징 쿼리를 위한 pageNum과 cpp
-		data.put("bno", bno); // 몇 번 글의 댓글을 가져올지에 대한 정보
+		data.put("paging", vo); //페이징 쿼리를 위한 pageNum과 cpp
+		data.put("bno", bno); //몇 번 글의 댓글을 가져올지에 대한 정보
 		
 		return mapper.getList(data);
 	}
 
 	@Override
 	public int getTotal(int bno) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mapper.getTotal(bno);
 	}
 
 	@Override
